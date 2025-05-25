15f87170b401b3291be2f34ab5090855ee400a44
woodenkolobok <kirillshamanskii38@gmail.com>
2025-05-25 15:18:01 +0800

    pon

--- js/questions.js
+++ js/questions.js
@@ -18,7 +18,7 @@ const questions = [
             "1997г",
             "1991г"
         ],
-        correct_answer: 1,
+        correct_answer: 3,
         valor: 5
     },
     {
@@ -40,24 +40,49 @@ const questions = [
             "ascil_lowercase из модуля string",
             "написать в ручную"
         ],
-        correct_answer: 3,
+        correct_answer: 2,
         valor: 1
     },
 ]
 
 var currentQuestion = 0;
+var answerGiven = false;
+var valor = 0;
+
+const nextButton = document.getElementById('next_button');
+const skipButton = document.getElementById('skip_button');
+
 
 function selectAnswer(event, correct_answer){
+    if (answerGiven)
+        return
+
+    answerGiven = true;
     const target = event.target;
 
-    if (correct_answer == target.getAttribute('data-answer-index'))
+    nextButton.classList.remove('hidden')
+    skipButton.classList.add('hidden')
+
+    if (correct_answer == target.getAttribute('data-answer-index')){
         target.classList.add("correct")
-    else
+        target.classList.remove('neutral')
+        valor += 1
+    }
+        
+    else {
         target.classList.add("wrong")
+        const correctAnswerDiv = document.getElementById('correct_answer')
+        correctAnswerDiv.classList.add('correct')
+    }
 
 }
 
 function newQuestion(){
+    answerGiven = false
+
+    nextButton.classList.add('hidden')
+    skipButton.classList.remove('hidden')
+
     const questionsContainer = document.getElementById("question_container")
     const answersContainer = document.getElementById("answers_container")
 
@@ -74,14 +99,22 @@ function newQuestion(){
         const answer = question.answers[i]
         const newAnswer = document.createElement('div')
         newAnswer.classList.add('answer')
+        newAnswer.classList.add('neutral')
         newAnswer.setAttribute("data-answer-index", i)
         newAnswer.innerText = answer
         newAnswer.setAttribute('onclick', `selectAnswer(event, ${question.correct_answer})`)
+        if (i == question.correct_answer)
+            newAnswer.id = 'correct_answer'
         answersContainer.append(newAnswer)
     }
 }
 
 function skip(){
+    currentQuestion += 1
+    newQuestion()
+}
+
+function next(){
     currentQuestion += 1
     newQuestion()
 }
